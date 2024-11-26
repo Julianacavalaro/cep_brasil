4f6415ca95f77e78f10444e8bfdf640d27e00e55
julianacavalaro <juliana.cavalaro@hotmail.com>
2024-11-25 22:36:22 -0300

    Cria home

---
+++ cep_brasil/lib/feature/presenter/page/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {  
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Consumo de API"),
+        backgroundColor: Colors.purple,
+      ),
+      body:Container(
+        child: Center(
+          child: Column(
+            children: [
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Digite um cep:'),
+                style: TextStyle(fontSize: 16, color: Colors.blueAccent),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );  
+  }
+}
